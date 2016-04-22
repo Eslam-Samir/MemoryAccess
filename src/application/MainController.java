b036df34c3023893cdc5e9eed081b7a43d9e8441
manarsherif <manar.sherif93@gmail.com>
2016-04-22 23:19:33 +0200

    GUI

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,9 +1,18 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class MainController {
-	public void pressButton(ActionEvent event) {
+	@FXML
+	private int size;
+	@FXML
+	private int startingAddress;
+	
+	public void pressAdd(ActionEvent event) {
+		
+	}
+	public void pressNext(ActionEvent event) {
 		
 	}
 
