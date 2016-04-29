0e9bbfc623dbcf9bf70e511992d2ba25f44e7692
manarsherif <manar.sherif93@gmail.com>
2016-04-29 20:12:56 +0200

    GUI

--- src/application/TablesController.java
+++ src/application/TablesController.java
@@ -1,11 +1,14 @@
 package application;
 
+import java.io.Console;
+import java.lang.invoke.ConstantCallSite;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -41,7 +44,6 @@ public class TablesController implements Initializable{
 	private String allocationType;
 	private int numberOfProcesses;
 	private ArrayList<ArrayList<Process>> output;
-	private ArrayList<Process> arrayListOfThisStage;
 	private int index;
 
 	
@@ -56,6 +58,16 @@ public class TablesController implements Initializable{
 		this.allocationType = allocationType;
 		System.out.println(allocationType);
 		InitializeAllocationType();
+		setList();
+		for(int i=0;i<list.size();i++){
+			System.out.println("Adress: "+list.get(i).getBaseAddress()
+					+"Name: "+list.get(i).getName()
+					+"size: "+list.get(i).getSize()
+					+"Type:"+list.get(i).getType()+"\t");
+		
+		}
+	//table.setItems(list);
+		
 	}
 	
 	public int getNumberOfProcesses() {
@@ -75,38 +87,46 @@ public class TablesController implements Initializable{
 		processes.add(new Process(name, size,0,ProcessType.process));
 		System.out.println("Added Process: "+name+ "\t" + String.valueOf(size));
 	}
-
+	public void pressNext(ActionEvent event) {
+		setList();
+		nameColumn.setCellValueFactory(new PropertyValueFactory<Process, String>("name"));
+		sizeColumn.setCellValueFactory(new PropertyValueFactory<Process, Integer>("size"));
+		baseAddressColumn.setCellValueFactory(new PropertyValueFactory<Process, Integer>("baseAdress"));	
+		table.setItems(list);
+		StageNumber.setText(m1.getStages().get(index));
+		
+	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		nameColumn.setCellValueFactory(new PropertyValueFactory<Process, String>("name"));
 		sizeColumn.setCellValueFactory(new PropertyValueFactory<Process, Integer>("size"));
-		baseAddressColumn.setCellValueFactory(new PropertyValueFactory<Process, Integer>("baseAdress"));	
+		baseAddressColumn.setCellValueFactory(new PropertyValueFactory<Process, Integer>("baseAdress"));
+		
 	}
 	
 	private void InitializeAllocationType() {
-		if (allocationType=="First Fit")
+		if (allocationType==Contstant.FF)
 			{
 				m1=new FirstFitAllocator(numberOfProcesses, processes, holes);
 			}
-		else if(allocationType=="Best Fit")
+		else if(allocationType==Contstant.BF)
 			{
 				m1=new BestFitAllocator(numberOfProcesses, processes, holes);
 			}
-		else if(allocationType=="Worst Fit")
+		else if(allocationType==Contstant.WF)
 			{
 				m1=new WorstFitAllocator(numberOfProcesses, processes, holes);
 			}
 		m1.RunAllocator();
 		output=m1.getOutput();
 		index=0;
-		arrayListOfThisStage=output.get(index);
 		
 	}
 	
 	
 	public void setList() {
-		for(int i=0;i<arrayListOfThisStage.size();i++){
-			list.add(arrayListOfThisStage.get(i));
+		for(int i=0;i<output.get(index).size();i++){
+			list.add(output.get(index).get(i));
 		}
 		index++;
 		
